b05005d96b7ccec4abceb953d7464fc4261b8def
Dmitry Rakovets <dmitryrakovets@gmail.com>
2022-06-09 05:29:58 +0300

    feat: add examples for theme 'Decision-making statements'

---
+++ src/decision_making_statements/Example1IfStatement.java
@@ -0,0 +1,12 @@
+package decision_making_statements;
+
+public class Example1IfStatement {
+    public static void main(String[] args) {
+        int number = -10;
+
+        if (number > 0) {
+            System.out.println("This number is positive");
+        }
+        System.out.println("Statement outside if block");
+    }
+}
